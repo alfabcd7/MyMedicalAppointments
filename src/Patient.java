c52a57ca0acc6c41ef08432cc3b86219ad8741c3
Alfredo Soria <carlos.alfredo.soria@gmail.com>
2020-07-14 04:23:33 -0400

    Se Creo la clase Patient

---
+++ src/Patient.java
@@ -0,0 +1,17 @@
+public class Patient {
+    static int id;
+    String name;
+    String email;
+    String address;
+    String phoneNumber;
+    String birthday;
+    String blood;
+    double weight;
+    double height;
+
+    Patient(String name , String email){
+        this.name = name;
+        this.email = email;
+
+    }
+}
