e5bbf31a4f3ac218824869942cbb7cc1c8f4f09d
carlos alfredo soria añez <carlos.alfredo.soria@gmail.com>
2020-09-24 12:53:55 -0400

    Se modifico las clases main , doctor , uiDoctorMenu y UiMenu.....Agregando la documentacion del codigo y areglando algunos detalles de sintaxis. [Falta p^C trabajar]

--- src/ui/UiDoctorMenu.java
+++ src/ui/UiDoctorMenu.java
@@ -1,8 +1,13 @@
+/*
+OBSERVACIONES : Cosas para agregar a futuro.
+1.- La fecha se repite , Ya no deberia pedirle si en algun momento ya la hemos seleccionado.
+2.- Falta validacion de informacion ingresada.
+
+ */
+
 package ui;
-
-
 import model.Doctor;
-
+import static ui.UiMenu.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -12,15 +17,21 @@ public class UiDoctorMenu {
 
 
 
-    public static void showDoctorMenu(){
+    public static void showDoctorMenu(){ //Este metodo se encarga de mostrar la logica de opciones para el doctor logeado
         int response = 0;
-        do {
+        do { // imprime el menu de Doctor.
             System.out.println("\n\n");
             System.out.println("Doctor");
-            System.out.println("Welcome " + UiMenu.doctorLogged.getName());
+            /* A continuacion llama la variable publica y estatica de la clase UiMenu "doctorLogged" (q es donde se contruyo el objeto Doctor en especifico),
+            Ademas de asignada a la variable mencionada para que pueda ser consumida en esta clase en especifico.*/
+            System.out.println("Welcome " + doctorLogged.getName());
+            /*
+            A continuacion esta Opcion permite al Doctor especificar las fechas en la que el esta disponible.
+            Para hacerlo llama al metodo de esta misma clase llamado : showAddAvailableAppointmentsMenu() [Ver codigo abajo.]
+             */
             System.out.println("1. add available Appointment");
-            System.out.println("2. My scheduled appointments");
-            System.out.println("0. Logout");
+            System.out.println("2. My scheduled appointments"); //Esta Opcion permite al Doctor permite ver el calendario fijado por cada paciente , en los horarios disponibles.
+            System.out.println("0. Logout");// deslogear.
 
             Scanner sc = new Scanner(System.in);
             response = Integer.valueOf(sc.nextLine());
@@ -32,56 +43,59 @@ public class UiDoctorMenu {
                 case 2 :
                     break;
                 case 0:
+                    showMenu(); // llama al metodo showMenu() de la clase UiMenu.
                     break;
             }
 
 
-        }while (response != 0);
+        }while (response != 0); // si la respuesta es diferente a cero , repite.
     }
  private static void showAddAvailableAppointmentsMenu(){
         int response = 0;
-        do {
+        do {// imprime el menu de opciones de [1. add available Appointment] "
             System.out.println();
-            System.out.println(":: add Available Appoinments");
-            System.out.println(" :: Select a Month");
+            System.out.println(":: add Available Appoinments"); // Impresion de titulo.
+            System.out.println(" :: Select a Month");// impresion de instrucciones
 
-            for (int i = 0; i < 3 ; i++) {
-                int j = i + 1;
-                System.out.println(j + "." + UiMenu.MONTHS[i]);
-            }
-            System.out.println("0. Return");
-
+               for (int i = 0; i < 12 ; i++) { // recorre 12 poscisiones , correspondiente a los 12 meses del año.
+                    int j = i + 1; // aumenta la numeracion en +1 , ya que este correspondera al indice que se imprimira por pantalla. (NO puede empezar en cero.)
+                    System.out.println(j + "." + MONTHS[i]);
+               }
+            System.out.println("0. Return"); // Ingresa 0 para retornar al menu anterior.
             Scanner sc = new Scanner(System.in);
-            response = Integer.valueOf(sc.nextLine());
-
-            if (response > 0 && response < 4){
-                int monthSelected = response;
-                System.out.println(monthSelected + "." + UiMenu.MONTHS[monthSelected-1]);
-
-                System.out.println("Insert the date available : [dd/mmm/yyyy]");
-                String date = sc.nextLine();
-                System.out.println("Your date is : " + date + "\n1. Correct \n2. Change Date");
-                int responseDate = Integer.valueOf(sc.nextLine());
-                if (responseDate == 2 ) continue;
-
-                int responseTime = 0;
-                String time = "";
-                do {
-                    System.out.println("Insert the time available for date " + date + "[16:00]");
-                    time = sc.nextLine();
-                    System.out.println("Your time is: " + time+"\n1. Correct \n2. Change time");
-                    responseTime = Integer.valueOf(sc.nextLine());
-                }while (responseTime == 2);
-                UiMenu.doctorLogged.addAvailableAppointment(date,time);
-                checkDoctorAvailableApppointments(UiMenu.doctorLogged);
-            }else if (response == 0){
-                showDoctorMenu();
-            }
-        }while (response != 0);
+            response = Integer.valueOf(sc.nextLine()); //recibe numero enteros.
+                if (response > 0 && response < 13){
+                    int monthSelected = response; //Asigno la respuesta en una variable cuyo nombre sea mas entendible.
+                    System.out.println(monthSelected + "." + MONTHS[monthSelected-1]); // Imprime el mes seleccionado.
+                    System.out.println("Insert the date available : [dd/mmm/yyyy]"); //insertar fechas disponibles para mostrar a los pacientes.
+                    String date = sc.nextLine(); // captura el String ingresado por terminal.
+                    /*
+                    EL usuario debe definir si los datos ingresados son correctos o incorrectos.
+                    */
+                    System.out.println("Your date is : " + date + "\n1. Correct \n2. Change Date");//imprime un menu para validar la informacion ingresada.
+                    int responseDate = Integer.valueOf(sc.nextLine()); // crea una nueva variable para capturar la respuesta de tipo entero.
+                    if (responseDate == 2 ) continue;//En caso de no ser correcta la fecha vuelve a la condicion expuesta en el ambito del primer "do-while".
+                    //--------------------------------------------//---------------------------------------------------------------
+                    //Creando variables para almacenar la respuesta de tiempo disponible del doctor.
+                    int responseTime = 0;
+                    String time = "";
+                    do { // En caso la fecha ingresada fue correcta , Imprime el menu para ingresar la "hora".
+                        System.out.println("Insert the time available for date " + date + "[16:00]"); // var date [linea 66]
+                        time = sc.nextLine(); //captura el string entrante.
+                        System.out.println("Your time is: " + time +"\n1. Correct \n2. Change time");
+                        responseTime = Integer.valueOf(sc.nextLine()); //recibe el sgte integer.
+                    }while (responseTime == 2);//mientras responseTime es igual a 2 , repite.
+            doctorLogged.addAvailableAppointment(date,time); //llama a un metodo en clas Doctor el cual agrega a un ArrayList el appointment el cual es una clase interna del mismo Doctor..
+            checkDoctorAvailableApppointments(doctorLogged); // Llama al metodo para agregar dentro de un ArrayList q describa una lista de Doctores q tengan citas disponibles.
+                    // el nombre del ArrayList es [doctorsAvailableAppointments].
+                }else if (response == 0){ // Si se Elijo Return.
+                    showDoctorMenu(); //Vuelve a llamar el menu de Doctores.
+                }
+        }while (response != 0);//Mientras la respuesta sea distinta de cero , repite.
  }
  private static void checkDoctorAvailableApppointments(Doctor doctor){
-        if (doctor.getAvailableAppointments().size() > 0 && !doctorsAvailableAppointments.contains(doctor)){
-            doctorsAvailableAppointments.add(doctor);
+        if (doctor.getAvailableAppointments().size() > 0 && !doctorsAvailableAppointments.contains(doctor)){ //Si el Doctor si tiene citas y si no existe en el arreglo doctorsAvailableAppointments
+            doctorsAvailableAppointments.add(doctor); //agregar ese doctor.
         }
 
  }
--- src/ui/UiMenu.java
+++ src/ui/UiMenu.java
@@ -2,31 +2,37 @@ package ui;
 
 import model.Doctor;
 import model.Patient;
-
+import static ui.UiDoctorMenu.*;
+import static ui.UiPatientMenu.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class UiMenu {
-    // final permite que su valor nunk sea reasignado
     public static final String [] MONTHS = {"Enero" ,"Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
-    public static Doctor doctorLogged;
-    public static Patient patientLogged;
+    public static Doctor doctorLogged; //Permitira llamar la variable de tipo Doctor(objeto) desde cualquier clase (Static).
+    public static Patient patientLogged;//Permitira llamar la variable de tipo Patient(objeto) desde cualquier clase (Static).
 
     // showMenu Methods
     public static void showMenu(){
+        //Saludo//
         System.out.println("Welcome to My Appointments");
         System.out.println("Selecciona la opción deseada");
-
         int response = 0;
-        do {
+        do { // Despliega opciones
             System.out.println("1. Doctor");
             System.out.println("2. Patient");
             System.out.println("0. Salir");
-
             Scanner sc = new Scanner(System.in);
-            response = Integer.valueOf(sc.nextLine());
-
+            response = Integer.valueOf(sc.nextLine()); //captura respuesta
             switch (response){
+                /*
+                Metodo authUser (Metodo que simulara la autentificacion de Usuario (Es Privado y Estatico).)
+                <<Parametros>>
+                userTye = 1 : Doctor
+                userType = 2 Patient
+                 */
+
                 case 1:
                     System.out.println("model.Doctor");
                     authUser(1);
@@ -36,59 +42,63 @@ public class UiMenu {
                     response = 0;
                     authUser(2);
                     break;
-                case 0:
+                case 0: //Sake de la aplicacion
                     System.out.println("Thank you for you visit");
                     break;
-                default:
+                default: // En caso no sea ni 0 ,1 y 2.
                     System.out.println("Please select a correct answer");
             }
-        }while (response != 0);
+        }while (response != 0); //Mientras no sea cero , Repite.
     }
 
 
-    private static void authUser(int userType){
-        //userTye = 1 : Doctor
-        //userType = 2 Patient
-
-        ArrayList <Doctor> doctors  = new ArrayList<>();
+    private static void authUser(int userType){ //Metodo que simulara la autentificacion de Usuario (Es Privado y Estatico).
+        /* Reglas del Metodo , Si:
+           1.- UserTye = 1 : Doctor.
+           2.- userType = 2 Patient.
+         */
+        //--------------------------------------------//---------------------------------------------------------------
+         //Simulacion de lista de Doctores Registrados.
+        ArrayList <Doctor> doctors  = new ArrayList<>(); //lista que recibe datos de tipo "Doctor". (el tamaño es automatico).
+        //Ingresando datos a la lista "doctors":
         doctors.add(new Doctor("Carlos Alfredo Soria Escalante" , "alfredo.soria@gmail.com" , "71696273"));
         doctors.add(new Doctor("Jorge Babdour" , " " , " "));
         doctors.add(new Doctor("Karen Sosa" , "Kare@gmail.com" , " "));
-
-        ArrayList<Patient> patients = new ArrayList <> ();
-
+        //--------------------------------------------//---------------------------------------------------------------
+        //Simulacion de lista de Pacientes Registrados.
+        ArrayList<Patient> patients = new ArrayList <> ();//lista que recibe datos de tipo "Patiente". (el tamaño es automatico).
+        //Ingresando datos a la lista "patients":
         patients.add(new Patient("Carlos Alfredo Soria Añez" , "carlos.alfredo.soria@gmail.com" , "77047779"));
         patients.add(new Patient("Gloria Añez Ribera" , "laprofegloria@gmail.com " , "72600448"));
         patients.add(new Patient("Rosa Maria Montaño Del Alcazar" , "montanorosi@gmail.com" , "75024644"));
-
-        boolean emailCorrect = false ;
+        //Validacion del Email.-----------------------//---------------------------------------------------------------
+        boolean emailCorrect = false ; // Variable booleana que identificara si el email ingresa es correcto // por defecto False.
        do {
-           System.out.println("Insert your email: [a@a.com]");
+           System.out.println("Insert your email: Example --> [a@a.com]"); //
            Scanner sc = new Scanner(System.in);
-           String email = sc.nextLine();
-           if(userType == 1){
-                   for (Doctor d: doctors){
-                       if (d.getEmail().equals(email)){
-                           emailCorrect = true;
-                           //obtener el usuario logeado
-                           doctorLogged = d;
+           String email = sc.nextLine(); // Recibe los caracteres de la terminal de Entrada.
+           if(userType == 1){ // Si es es un tipo Doctor (la recibe desde el metodo showMenu)
+                   for (Doctor d: doctors){ //recorre el ArrayList "doctors".
+                       if (d.getEmail().equals(email)){ // si el atributo "email" del objeto Doctor es igual (equals) a el "email" ingresado.
+                           emailCorrect = true; // cambia a true (se cumplio).
+                           doctorLogged = d; //obtener el usuario logeado. //pasa la referencia de la variable "d" de tipo Doctor proveniente del Arraylist hacia otra var. del mismo tipo.
                            //showdoctorMenu
-                           UiDoctorMenu.showDoctorMenu();
+                        showDoctorMenu(); //llamada al metodo de la clase UiDoctorMenu.
                        }
                    }
            }
-           else if (userType == 2){
-               for (Patient p: patients){
-                   if (p.getEmail().equals(email)){
-                       emailCorrect = true;
-                       //obtener el usuario logeado
-                       patientLogged = p;
-                       //showdoctorMenu
+           else if (userType == 2){ // Si es es un tipo Patient (la recibe desde el metodo showMenu)
+               for (Patient p: patients){  //recorre el ArrayList "patients".
+                   if (p.getEmail().equals(email)){ // si el atributo "email" del objeto Patient es igual (equals) a el "email" ingresado.
+                       emailCorrect = true;// cambia a true (se cumplio).
+                       patientLogged = p; //obtener el usuario logeado. // pasa la referencia de la variable "p" de tipo Patient proveniente del Arraylist hacia otra var. del mismo tipo.
+                       //showpatientMenu
+                       showPatientMenu(); //llamada al metodo de la clase UiPatientMenu.
                    }
                }
 
            }
-       }while (!emailCorrect);
+       }while (!emailCorrect); //Si email es Falso ; Repite.
     }
 
 
