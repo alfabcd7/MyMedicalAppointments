eef00dde862313ab10d64ef9a816e7e6007065a0
Carlos Alfredo Soria Añez <carlos.alfredo.soria@gmail.com>
2020-10-12 23:12:55 -0400

    Agrego modificaciones en el idioma ingles que describen los distintos menus e ingreso documentacion al codigo de la clase UiDoctorMenu , metodo showAddAvailableAppointment , aun falta terminar documentacion desde Insertion Time , esta corriendo bien el metodo.

--- src/ui/UiDoctorMenu.java
+++ src/ui/UiDoctorMenu.java
@@ -4,12 +4,9 @@ OBSERVACIONES : Cosas para agregar a futuro.
 2.- Falta validacion de informacion ingresada.
 
  */
-
 package ui;
 import model.Doctor;
 import static ui.UiMenu.*;
-
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -27,14 +24,14 @@ public class UiDoctorMenu {
         do { // Inicio del "do" cuyo cuerpo estara encargado de imprimir el "menu Doctor".
             System.out.println("\n\n");
             //Se consume doctorLogged llamando al metodo q obtiene el atributo name del doctor en especifico.// doctorLogged es llamado desde ui.Menu
-            System.out.println("Welcome Dr " + doctorLogged.getName() + " Please , Choose an option.");
-            System.out.println("1. add available Appointment"); // Opcion para llamar al metodo "showAddAvailableAppointmentsMenu"
-            System.out.println("2. My scheduled appointments"); //Opcion permite al Doctor permite ver el calendario fijado por cada paciente , en los horarios disponibles.
-            System.out.println("0. Logout");// salir de la sesion
-
+            System.out.println("::Welcome Dr. " + doctorLogged.getName() + " Please,Choose one of the following options.");
+            System.out.println(">1.-Add an Available Appointment"); // Opcion para llamar al metodo "showAddAvailableAppointmentsMenu"
+            System.out.println(">2.-Watch My Scheduled Appointments Dr."); //Opcion permite al Doctor permite ver el calendario fijado por cada paciente , en los horarios disponibles.
+            System.out.println(">0.-Logout.");// salir de la sesion
+            //..
             Scanner sc = new Scanner(System.in);
             response = Integer.valueOf(sc.nextLine());
-
+            //..
             switch (response) {
                 case 1:
                     showAddAvailableAppointmentsMenu();
@@ -43,17 +40,17 @@ public class UiDoctorMenu {
                     break;
                 case 0:
                     do { //Bucle para Opciones de Logout
-                        System.out.println(":: Are you sure , do you want to logout?");
-                        System.out.println("\n 1.- Yes \n 2.- No");
+                        System.out.println("::¿Are you sure that do you want to logout?");
+                        System.out.println("\n>1.-Yes \n>2.-No");
                         response = Integer.valueOf(sc.nextLine());
                         System.out.println("\n");
                         if (response == 1) { //SI la Respuesta es SI deseo salir
-                            System.out.println("\n :: Thank you For your Visit, Dr " + doctorLogged.getName() + " , See you later \n\n");
+                            System.out.println("\n ::Thank you For your Visit,Dr. " + doctorLogged.getName() + " ,See you later \n\n");
                             showMenu(); // llama al metodo showMenu() de la clase UiMenu.
                         } else if (response == 2) { //Si es NO , Sale del Bucle Y vuelve al anterior Bucle de Menu Doctor
-                            System.out.println("::Catch it \n\n");
+                            System.out.println("::¡¡I Got iT!!\n\n");
                         } else {
-                            System.out.println(":: Error , that is not an Correct Option , Try Again");
+                            System.out.println("::Error,That is not an Available Option,Try Again");
                         }
                     }while (response != 2) ; // Mientras sea diferente de 2 , repite.
                     break;
@@ -61,47 +58,52 @@ public class UiDoctorMenu {
         }while (response != 0) ; // si la respuesta es diferente a cero , repite.
     }
  private static void showAddAvailableAppointmentsMenu() {
-     boolean selectionMonth = false;
-     int response = 0;
-     String [] text = new String [10] ;
-     text[0] = "::You have chosen the following Option ::";
-     text[1] = "\n  >1. Correct \n  >2. No , I want to Change \n  >3. I want to go back to the month selection menu";
-     text[2] = "\n::Backing to the Doctor Menu";
-     text[3] = "      >I got you!!,Selection confirmed, So lets move on!!\n";
-     text[4] = "    >Ok , No Problem , You can change it!!";
-     text[5] = ">!Ok¡ , No Problem , Lest Go To the month selection menu!!";
-     text[6] = "\n:: Error , that is not an Correct Option , Try Again" ;
-     do {// Incio "Do - while" de Selection Month.
-         boolean   confirmationTime = false , insertionDate = false , insertionTime = false , confirmationDate = false ;
-         selectionMonth = false;
-         System.out.println();
-         System.out.println("::Welcome to add Available Appoinment option Menu"); // Impresion de titulo.
-         System.out.println("::Please , Select a Month");// impresion de instrucciones
+     boolean selectionMonth = false; // Variable que da control al Do mas superficial.
+     int response = 0; // variable para capturar la respuesta.
+     String [] text = new String [10] ;//Array Para almacenar los String repetitivos.
+     text[0] = "::You have chosen the following Option :";
+     text[1] = "\n>1.-Correct\n>2.-No,I want to Change\n>3.-I want to go back to the Month selection menu";
+     text[2] = "\n::Backing to Doctor Menu";
+     text[3] = "      >\n¡¡I got it!!,¡¡Selection confirmed, So let's move on!!";
+     text[4] = "      >\nOk,No Problem,You can change it!!";
+     text[5] = ">¡Ok!,No Problem,¡¡Let's Go To the Month selection menu!!";
+     text[6] = "\n::¡¡Error!!,¡¡that is not a Correct Option,Try Again!!";
+     do {// Incio "Do - while" de Selection Month.//selectionMonth = false
+         boolean   confirmationTime = false , insertionDate = false , insertionTime = false , confirmationDate = false ;//Variables booleanas destinadas a la manipulacion del flujo del menu actual.
+         selectionMonth = false;//Reconfirmamos la variable en false.
+         System.out.println(" ");//..
+         System.out.println("::Welcome to the 'Add Available Appointment Option Menu'"); // Impresion de titulo.
+         System.out.println("::Please,Select a Month: ");// impresion de instrucciones
          //...
          for (int i = 0; i < 12; i++) { // recorre 12 poscisiones que corresponden a los 12 meses del año.
              int j = i + 1; // aumenta la numeracion en +1 , corresponde al indice que se imprime por pantalla. (NO puede empezar en cero.)
-             System.out.println("    >" + j + "." + MONTHS[i]); // Imprime los dos meses.
+             System.out.println("    >" + j + ".-" + MONTHS[i]); // Imprime los dos meses.
          }
-         System.out.println(">0. Return"); // Opcion para retornar al menu.
+         System.out.println(">0.-Return"); // Opcion para retornar al menu.
          //..
          Scanner sc = new Scanner(System.in); // Clase Scanner.
-         response = Integer.valueOf(sc.nextLine()); //Variable que recibe la respuesta.
+         response = Integer.valueOf(sc.nextLine());//Variable que recibe la respuesta.
          //...
-         if (response > 0 && response < 13) { // Si response es entre 1 y 12 ...Entra al bucle.
+         if (response > 0 && response < 13) { // Si la respuesta esta entre 1 y 12 ...Entra en un bucle.
              int monthSelected = response; //Asigno la respuesta en una variable cuyo nombre sea mas entendible.
-             do {// Inicio de "Do-While" de Insertion Date
-                 insertionDate = false;
-                 System.out.println(text[0] + monthSelected + "." + MONTHS[monthSelected - 1]); // Imprime el mes seleccionado.
-                 System.out.println("::You have confirmated the Month , so...We are going to Insert a date now.");
-                 System.out.println("::Please , Insert the date available to " + MONTHS[monthSelected - 1] + " Example --> [dd/mm/yyyy]"); //insertar fechas disponibles para mostrar a los pacientes.
-                 String dateStringCatch = sc.nextLine(); // captura el String ingresado por terminal.
+             do {// Inicio de "Do-While" de Insertion Date//insertionDate = false;
+                 insertionDate = false;//Reconfirmamos la variable para el area de Insertion Date en false.
+                 System.out.println(text[0] +" >" + monthSelected + ".-" + MONTHS[monthSelected - 1]); // Imprime el mes seleccionado.
+                 System.out.println("::You have confirmed the Month,So we are going to Insert a date now.");
+                 System.out.println("::Please , Insert the date available to " + MONTHS[monthSelected - 1] + " Format Example --> [dd/mm/yyyy]"); //insertar fechas disponibles para mostrar a los pacientes.
+                 String dateStringCatch = sc.nextLine(); // captura el String Date insertado por terminal.
                  do { // Inicio de "Do-While" de Confirmation Date
                      /* el usuario debe definir si los datos ingresados son correctos o incorrectos.*/
-                     confirmationDate = false;
+                     confirmationDate = false; //Reconfirmamos la variable para el area de Confirmation Date en false.
                      System.out.println(text[0] + " date : " + dateStringCatch + text[1]);//imprime un menu para validar la informacion ingresada.
                      response = Integer.valueOf(sc.nextLine()); // vuelve a capturar la respuesta en la misma variable response.
                      switch (response) { // Inicio de "Switch" de Confirmation Date
-                         case 1:
+                         /*confirmationDate = True : Salimos del Bucle "Do-While" de Confirmation Date
+                           confirmationDate = False : Mantenemos el Bucle "Do-While" de Confirmation Date
+                           insertionDate = True : Salimos del Bucle "Do-While" de Insertion Date
+                           insertionDate = False : Mantenemos el Bucle "Do-While" de Insertion Date
+                          */
+                         case 1://>1.-Correct
                              int responseTime = 0;
                              String timeStringCatch = "";
                              System.out.println(text[3]); //"      I got you!!,Selection confirmed, So lets move on!!\n"
@@ -177,28 +179,28 @@ public class UiDoctorMenu {
                                  } while (!confirmationTime); // fin de "Do - while" de confirmation Time
                              }while (!insertionTime);// Fin de "Do-while" de Insertion Time
                              break;
-                         case 2:
+                         case 2://>2.-No,I want to Change
                              System.out.println(text[4]);
                              confirmationDate = true;
                              break;
-                         case 3:
+                         case 3://>3.-I want to go back to the Month selection menu"
                              System.out.println(text[5]);
                              confirmationDate = true;
                              insertionDate = true;
                              break;
-                         default:
-                             System.out.println(text[6]);
+                         default: //otra respuesta que no sea 1,2 o 3
+                             System.out.println(text[6]);//::¡¡Error!!,¡¡that is not a Correct Option,Try Again!!"
                              confirmationDate = false;
                              break;
                      }// Fin de "Switch" de Confirmation Date
                  } while (!confirmationDate); //fin de "Do-while" de Confirmation Date.
              }while(!insertionDate); //fin de "Do-while" de Insertion Date.
          }else if(response == 0) { // Si se Elijo Return.
-             System.out.println(text[2]);
-             selectionMonth = true ;
+             System.out.println(text[2]);//::Backing to Doctor Menu";
+             selectionMonth = true ; //Salimos del Bucle "Do-While" de Selection Month
          } else {
-             System.out.println(text[6]);
-             selectionMonth = true;
+             System.out.println(text[6]);//::¡¡Error!!,¡¡that is not a Correct Option,Try Again!!"
+             selectionMonth = false; //Nos mantenemos en el Bucle "Do-While" de Selection Month
          }
      }while (!selectionMonth); // Fin "Do - while" de Selection Month.
      showDoctorMenu(); //Vuelve a llamar el menu de Doctores.
@@ -207,23 +209,6 @@ public class UiDoctorMenu {
         if (doctor.getAvailableAppointments().size() > 0 && !doctorsAvailableAppointments.contains(doctor)){ //Si el Doctor si tiene citas y si no existe en el arreglo doctorsAvailableAppointments
             doctorsAvailableAppointments.add(doctor); //agregar ese doctor.
         }
-
- }
- private void printConfirmationDateTime(boolean date , boolean time , int response){
-        /* date = true , Tomara encuenta los textos para date.
-        *  date = false ,No Tomara encuenta los textos para date.
-        *time = true , Tomara encuenta los textos para time.
-        * time = false , No Tomara encuenta los textos para time.
-        *  */
-
-
-
-     if (date == true && time == false) {
-
-
-     }
-
-
  }
 
 
